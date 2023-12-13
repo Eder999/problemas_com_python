c6b448925cd88af127c3354807d412c821cc795a
Eder999 <eder.jose.a.oliveira@hotmail.com>
2023-12-13 12:23:01 -0300

    Fibonacci 6
    Signed-off-by: Eder999 <eder.jose.a.oliveira@hotmail.com>

---
+++ compression/trivial_compression.py
@@ -0,0 +1,8 @@
+class CompressedGene:
+  def __init__(self, gene: str) -> None:
+    self._compress(gene)
+
+def _compress(self, gene: str) -> None:
+  self.bit_string: int 1
+  for nucleotide in gene.upper():
+    self.bit_string <<= 2
